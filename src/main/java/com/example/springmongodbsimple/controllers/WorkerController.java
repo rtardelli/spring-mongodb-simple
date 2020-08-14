7551ff1e20d8189763cbf6b68da26f6c108de05f
Rodrigo Tardelli <rodrigotmg@gmail.com>
2020-08-14 14:50:03 -0300

    Inserted log on validation errors

--- src/main/java/com/example/springmongodbsimple/controllers/WorkerController.java
+++ src/main/java/com/example/springmongodbsimple/controllers/WorkerController.java
@@ -84,14 +84,14 @@ public class WorkerController {
 
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public Map<String, String> handleValidationExceptions(
-    MethodArgumentNotValidException ex) {
+  public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
     Map<String, String> errors = new HashMap<>();
     ex.getBindingResult().getAllErrors().forEach((error) -> {
       String fieldName = ((FieldError) error).getField();
       String errorMessage = error.getDefaultMessage();
       errors.put(fieldName, errorMessage);
     });
+    LOG.error("Errors found in request {}: {}", ex.getParameter(), errors);
     return errors;
   }
 }
